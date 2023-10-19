68222a5e8598a2f97117ed30e80c577cbbf9e959
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2023-10-18 21:06:50 -0500

    feat: agrega clase gato

---
+++ src/main/java/com/ilanangelesrodriguez/Gato.java
@@ -0,0 +1,40 @@
+package com.ilanangelesrodriguez;
+
+public class Gato {
+    int id;
+    String url;
+    String apikey = "live_12uBcrcKucmZQIPAZyBWWlMpacyah9xoh4vN32Sp6zZCyrhFDfUHWjtxtH5Uuarw";
+    String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
